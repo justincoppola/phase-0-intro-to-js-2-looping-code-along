188569e650c020f55c49b1f27a1d07395f5160ee
justin <justincoppola6@gmail.com>
2023-06-19 15:11:34 -0400

    fixed all code to work properly

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "wonderful surprise gift!"; 
+const thankYouMessages = writeCards(names, eventName);
+console.log(thankYouMessages);
+
+countDown(5);
